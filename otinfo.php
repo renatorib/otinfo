53ba38a64c994b86fa5b3c7eb5693f08471cd661
Ranieri Althoff <ranisalt@gmail.com>
2014-05-05 02:44:28 -0300

    Changed cache mode from ini to json. Now using dataset array instead of separate variables for information, static variables for better performance and dynamic getters and setters for flexibility.

--- otinfo.php
+++ otinfo.php
@@ -3,8 +3,9 @@
 /**
  * @package Open Tibia Information <otinfo>
  * @version 1.0
- * @author Renato Ribeiro <renatoribeiro@tibiaking.com> with a some lines of Robson Dias
- * @copyright 2014 (C) by Renato Ribeiro <http://tibiaking.com/>
+ * @author Renato Ribeiro <renatoribeiro@tibiaking.com>
+ * @author Ranieri Althoff <ranieri@inf.ufsc.br>
+ * @copyright 2014 (C) by Renato Ribeiro <http://tibiaking.com>, Ranieri Althoff <http://otserv.com.br>
  * @license http://www.gnu.org/licenses/lgpl-3.0.txt GNU Lesser General Public License, Version 3
  * 
  * Official repository: https://github.com/renatorib/otinfo
@@ -13,164 +14,146 @@
 
 class Otinfo {
 
-	/**
-	 * var connect
-	 * bool
-	 * verify if socket connect
-	 * if false, server offline or doest exists
-	 *
-	 */
-	public $connect;
+    /* Server attributes. */
+    private $attributes;
+    
+    /* Server connection information. */
+    private static $host;
+    private static $port;
 
-	/**
-	 * @serverinfo
-	 *
-	 */
-
-	public $host;
-	public $uptime;
-	public $ip;
-	public $servername;
-	public $port;
-	public $location;
-	public $url;
-	public $server;
-	public $version;
-	public $client;
-	public $motd;
-	public $owner_name;
-	public $owner_email;
-	public $players_online;
-	public $players_max;
-	public $players_peak;
-	public $monsters;
-	public $map_name;
-	public $map_author;
-	public $map_width;
-	public $map_height;
-	public $rates_experience;
-	public $rates_skill;
-	public $rates_magic;
-	public $rates_loot;
-	public $rates_spawn;
-
-	/**
-	 * var cache_check
-	 * int
-	 * time to recache informations
-	 * 1 = 1 second
-	 *
-	 * var cache
-	 * bool
-	 * allows cache information
-	 *
-	 */
-
-	public $cache = true; // highly recommended
-	public $cache_check = 120; // two minutes to recache
-
+    /* Cache time in seconds. Set to zero if unwanted. */
+	private static $cache = 120;
 	
-	function __construct($host, $port){
+	/* Magic message - what makes Open Tibia servers answer with their info. */
+	private static $message;
+	
+	function __construct($host, $port = 7171) {
+		static::$host = $host;
+        static::$port = $port;
+        
+        /* We initialize here since PHP does not support non-trivial initializers */
+		static::$message = chr(6).chr(0).chr(255).chr(255).'info';
+	}
+	
+	/**
+	 * Retrieve informations from cache or socket.
+	 *
+	 * @return bool
+	 */
+	public function execute() {
+	    
+	    /* Localization of the cache file */
+	    $cache_uri = 'cache' . DIRECTORY_SEPARATOR . static::$host . '.json';
+	    if (static::$cache && file_exists($cache_uri) && filemtime($cache_uri) + static::$cache < time()) {
+       	    $json = file_get_contents($cache_uri);
+       	    $this->attributes = json_decode($json);
+       	    return true;
+       	} else {
+       			
+	        /* Open socket with server */
+			if ($socket = @fsockopen(static::$host, static::$port, $errno, $errstr, 5)) {
 
-		$this->host = $host;
-
-		$ini = @parse_ini_file("cache" . DIRECTORY_SEPARATOR . "{$host}");
-		if(!$this->cache || !isset($ini['updated']) || $ini['updated'] + $this->cache_check < time()) {
-			$socket = fsockopen($host, $port, $errno, $errstr, 5);
-			if($socket){
+                /* Write magic string to the socket and get the response */
 				$data = '';
-				$this->connect = true;
-				fwrite($socket, chr(6).chr(0).chr(255).chr(255).'info');
+				
+				fwrite($socket, static::$message);
 				while(!feof($socket)) {
-					$data .= fgets($socket, 8192);
+					$data .= fread($socket, 1024);
 				}
 				fclose($socket);
-				$object = simplexml_load_string($data); // simplexml_load_string from php 5
-				$information = $this->_parseFromObject($object);
-				$cache = '';
-				foreach ($information as $index => $value){
-					$cache .= "{$index} = \"{$value}\"\n";
-				}
-				file_put_contents("cache" . DIRECTORY_SEPARATOR . "{$host}", $cache); // cache information
-				return @parse_ini_file("cache" . DIRECTORY_SEPARATOR . "{$host}");
-			} else {
-				$this->connect = false;
-				return false;
+				
+				/* Parse XML response */
+				$this->parseFromXml($data);
+				
+				/* Write response to a file if caching is enabled */
+                if (static::$cache && (is_dir('cache')) || mkdir('cache')) {
+                    file_put_contents($cache_uri, $this);
+                }
+                
+                return true;
 			}
-		} else {
-			$this->connect = true;
-			//var_dump($ini);
-			var_dump($this->_parseFromIni($ini));
-			return $this->_parseFromIni($ini);
+		}
+		return false;
+	}
+
+	private function parseFromXml($xml){
+		$array = simplexml_load_string($xml);
+
+        /* Check if is set and loop over the serverinfo node. */
+		if (isset($array->serverinfo)) {
+			foreach ($array->serverinfo->attributes() as $index => $value) {
+                $this->attributes['serverinfo'][$index] = (string)$value;
+            }
+		}
+
+        /* Check if is set and set the motd node. */
+		if (isset($array->motd)) {
+			$this->attributes['motd'] = (string)$array->motd;
+		}
+
+        /* Check if is set and loop over the owner node. */
+		if (isset($array->owner)) {
+   			foreach ($array->owner->attributes() as $index => $value) {
+		        $this->attributes['owner'][$index] = (string)$value;
+            }
+		}
+
+        /* Check if is set and loop over the players node. */
+		if (isset($array->players)) {
+			foreach ($array->players->attributes() as $index => $value) {            
+                $this->attributes['players'][$index] = (string)$value;
+            }
+		}
+
+        /* Check if is set and loop over the monsters node. */
+		if (isset($array->monsters)) {
+			foreach ($array->monsters->attributes() as $index => $value) {
+                $this->attributes['monsters'][$index] = (string)$value;
+            }
+		}
+
+        /* Check if is set and loop over the map node. */
+		if (isset($array->map)) {
+			foreach ($array->map->attributes() as $index => $value) {
+                $this->attributes['map'][$index] = (string)$value;
+            }
+		}
+
+        /* Check if is set and loop over the rates node. */
+		if (isset($array->rates)) {
+			foreach ($array->rates->attributes() as $index => $value) {
+                $this->attributes['rates'][$index] = (string)$value;
+            }
 		}
 	}
 
-	private function _parseFromObject($object){
-		$o = $object;
-		$return = array();
-
-		// Check & Loop @serverinfo node
-		if(isset($o->serverinfo)){
-			foreach($o->serverinfo->attributes() as $name => $info){
-				$this->{$name} = $info;
-				$return[$name] = $info;
-			}
-		}
-
-		// Check & set @motd node
-		if(isset($this->motd)){
-			$this->motd = $o->motd;
-			$return['motd'] = $o->motd;
-		}
-
-		// Check & Loop @owner node
-		if(isset($o->owner)){
-			foreach($o->owner->attributes() as $name => $info){
-				$this->owner_{$name} = $info;
-				$return['owner_'.$name] = $info;
-			}
-		}
-
-		// Check & Loop @players node
-		if(isset($o->players)){
-			foreach($o->players->attributes() as $name => $info){
-				$this->players_{$name} = $info;
-				$return['players_'.$name] = $info;
-			}
-		}
-
-		// Check & Set @monsters node
-		if(isset($this->monsters)){
-			$this->monsters = $o->monsters->attributes()['total'];
-			$return['monsters'] = $o->monsters->attributes()['total'];
-		}
-
-		// Check & Loop @map node
-		if(isset($this->map)){
-			foreach($o->map->attributes() as $name => $info){
-				$this->map_{$name} = $info;
-				$return['map_'.$name] = $info;
-			}
-		}
-
-		// Check & Loop @rates node
-		if(isset($o->rates)){
-			foreach($o->rates->attributes() as $name => $info){
-				$this->rates_{$name} = $info;
-				$return['rates_'.$name] = $info;
-			}
-		}
-
-		// Update time to cache
-		$return['updated'] = time();
-		return $return; //return array
+    /**
+	 * Dynamically retrieve attributes on the model.
+	 *
+	 * @param  string  $key
+	 * @return mixed
+	 */
+    public function __get($key) {
+		return $this->attributes[$key];
 	}
-
-	private function _parseFromIni($array){
-		foreach ($array as $key => $value) {
-			$this->{$key} = $value;
-		}
-		return $array;
+	
+	/**
+	 * Dynamically set attributes on the model.
+	 *
+	 * @param  string  $key
+	 * @param  mixed   $value
+	 * @return void
+	 */
+	public function __set($key, $value) {
+		$this->attributes[$key] = $value;
 	}
-
+	
+	/**
+	 * Convert the model to its string representation.
+	 *
+	 * @return string
+	 */
+	public function __toString() {
+		return json_encode($this->attributes);
+	}
 }
